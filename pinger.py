082a1e1a991af767162fc750a52f1c107b957ad6
unknown <denisasanov67@gmail.com>
2026-02-24 16:50:41 +0800

    Added last ava date

--- pinger.py
+++ pinger.py
@@ -8,7 +8,7 @@ while True:
     date = current.strftime("%Y-%m-$d")
     print(f"datetime {current} and {date}")
     url = url_template.format(date="2026-02-24")
-    response = requests.get(url = url, stream= True)
+    response = requests.get(url = url, stream= True) gti
     print(f"From url {url} got {response}")
     if response.status_code == 200:
         break
